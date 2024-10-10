7de76e351c725de06c494e5ea3a9a0ada9eaf67f
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-10-10 16:08:12 -0700

    mvp completed

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,7 +4,11 @@ const Recipe = require('./recipes-model')
 router.get('/:recipe_id', (req, res, next) => {
     Recipe.getRecipeById(req.params.recipe_id)
         .then(resource => {
-           
+           if (!resource || resource.length === 0) {
+            const error = new Error('Recipe ID not found')
+            error.status = 404
+            throw error
+           }
             res.status(200).json(resource)
         })
         .catch(next)
